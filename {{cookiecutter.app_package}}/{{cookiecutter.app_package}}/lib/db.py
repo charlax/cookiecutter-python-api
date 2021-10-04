34ce3aecdae1680be4e206a60faa3826bb7c3a10
Charles-Axel Dein <120501+charlax@users.noreply.github.com>
2021-10-04 15:57:01 +0200

    Improve lib/db

--- {{cookiecutter.app_package}}/{{cookiecutter.app_package}}/lib/db.py
+++ {{cookiecutter.app_package}}/{{cookiecutter.app_package}}/lib/db.py
@@ -1,3 +1,6 @@
+import importlib
+import pkgutil
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session
@@ -5,3 +8,23 @@ from sqlalchemy.orm import Session
 engine = create_engine("TODO", future=True)
 session = Session(engine)
 Base = declarative_base()
+
+
+def import_all_repos():
+    """Import all repos to make sure the mapper is fully initialized."""
+    from {{ cookiecutter.app_package}} import repo
+
+    def not_test(p):  # type: ignore
+        return "test" not in p.name
+
+    packages = pkgutil.walk_packages(repo.__path__, prefix=repo.__name__ + ".")  # type: ignore
+    for p in filter(not_test, packages):
+        importlib.import_module(p.name)
+
+
+def check_db() -> None:  # nocov
+    """Ensure the connection with the DB works."""
+    with engine.connect() as conn:
+        result = conn.execute("select 1")
+        value = result.fetchone()[0]
+        assert value == 1
